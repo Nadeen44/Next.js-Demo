eeb25ccc413910c471afa372d3b23f7fa97bdfd0
Nadeen44 <nadeenabdelrhman4@gmail.com>
2024-07-10 15:55:53 +0300

    customizing the not-found page for the app

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const notFoundPage = () => {
+  return (
+    <div><h5 className='justify-center text-center'>The requested page doesn't exist</h5></div>
+  )
+}
+
+export default notFoundPage
